08864f9473b432abe147dfa78b26454fc3fb6c70
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2024-08-09 12:36:27 -0400

    simplificando

--- configs/fullScreen.js
+++ configs/fullScreen.js
@@ -1,15 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Espera 5 segundos y luego intenta activar el fullscreen
-    setTimeout(() => {
-        // Verifica si el navegador soporta la API de Fullscreen
-        if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
-        } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-            document.documentElement.mozRequestFullScreen();
-        } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari y Opera
-            document.documentElement.webkitRequestFullscreen();
-        } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-            document.documentElement.msRequestFullscreen();
-        }
-    }, 5000); // 5000ms = 5 segundos
-});
+function goFullscreen() {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    }
+}
+
+// Wait 5 seconds and then enter fullscreen
+setTimeout(goFullscreen, 5000);
