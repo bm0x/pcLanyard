31bbfd49c59147aab5e56e54d777cfdde8c7fe2b
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2024-08-09 12:47:28 -0400

    prueba de fullscreen con video

--- configs/fullScreen.js
+++ configs/fullScreen.js
@@ -4,5 +4,12 @@ function goFullscreen() {
     }
 }
 
-// Wait 5 seconds and then enter fullscreen
-setTimeout(goFullscreen, 5000);
+// Reproduce un video en silencio para intentar habilitar fullscreen
+const video = document.getElementById('hidden-video');
+video.src = './video/videoplayback.mp4'; // Ruta a un pequeño video
+video.play().then(() => {
+    // Intenta entrar en fullscreen después de que el video comience a reproducirse
+    setTimeout(goFullscreen, 1000);
+}).catch(error => {
+    console.log('Error al reproducir el video:', error);
+});
