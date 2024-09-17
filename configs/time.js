166320e0f126f38718531c88cce003b7466d52ee
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2024-09-17 17:13:58 -0300

    formateando minutos

--- configs/time.js
+++ configs/time.js
@@ -8,7 +8,7 @@ function reloj() {
     var mes = meses[fecha.getMonth()];
     var año = fecha.getFullYear();
     var hora = fecha.getHours();
-    var minuto = fecha.getMinutes();
+    var minuto = fecha.getMinutes().toString().padStart(2, '0');;
 
     document.getElementById("fecha").innerHTML = dia + " de " + mes + " de " + año;
     document.getElementById("hora").innerHTML = hora + ":" + minuto;
