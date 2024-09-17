5e943e77f82c350a4ce94bac6493729db26669e8
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2024-09-17 17:15:20 -0300

    también dando formato a las horas

--- configs/time.js
+++ configs/time.js
@@ -7,8 +7,8 @@ function reloj() {
     var dia = fecha.getDate();
     var mes = meses[fecha.getMonth()];
     var año = fecha.getFullYear();
-    var hora = fecha.getHours();
-    var minuto = fecha.getMinutes().toString().padStart(2, '0');;
+    var hora = fecha.getHours().toString().padStart(2, '0');
+    var minuto = fecha.getMinutes().toString().padStart(2, '0');
 
     document.getElementById("fecha").innerHTML = dia + " de " + mes + " de " + año;
     document.getElementById("hora").innerHTML = hora + ":" + minuto;
